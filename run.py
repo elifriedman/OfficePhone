6c4606605f6a6d9a35b638a8b3619be28ed7f66b
elifriedman <friedm3@cooper.edu>
2016-10-30 02:17:14 -0400

    add recording

--- run.py
+++ run.py
@@ -1,14 +1,27 @@
-from flask import Flask
+from __future__ import with_statement   # Only necessary for Python 2.5
+from flask import Flask, request, redirect
 import twilio.twiml
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def hello_monkey():
-    """Respond to incoming requests."""
     resp = twilio.twiml.Response()
-    resp.say("Hello Monkey")
+    # Greet the caller by name
+    resp.say("Hello")
+    resp.record(maxLength="30", action="/handle-recording")
+    return str(resp)
 
+@app.route("/handle-recording", methods=['GET', 'POST'])
+def handle_recording():
+    """Play back the caller's recording."""
+
+    recording_url = request.values.get("RecordingUrl", None)
+
+    resp = twilio.twiml.Response()
+    resp.say("Thanks for howling... take a listen to what you howled.")
+    resp.play(recording_url)
+    resp.say("Goodbye.")
     return str(resp)
 
 if __name__ == "__main__":
